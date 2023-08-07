582b1116b9724ff3ebf0425ac51514b5553cc2a5
m-boga <muhammet.boga@hotmail.com>
2023-08-07 19:54:50 +0300

    EntityLayer Property Added. BusinessLayer Created. DataAccessLayer Created.

---
+++ EntityLayer/Concrete/AboutSecond.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class AboutSecond
+    {
+        public int AboutSecondId { get; set; }
+        public string TitleOne { get; set; }
+        public string TitleSecond { get; set; }
+        public string Description { get; set; }
+        public string  Image {  get; set; }
+
+
+    }
+}
---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string PostOneName { get; set; }
+        public string PostOneDescription { get; set; }
+        public string PostOneImage { get; set; }
+        public string Status { get; set; }
+
+
+    }
+}
